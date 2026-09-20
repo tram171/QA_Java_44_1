d04f3f03576a164df24040806f4cd60adf07cf06
tram_ <tram_171@mail.ru>
2026-09-20 22:58:05 +0700

    Added tests for text-box

---
+++ src/test/java/tests/simpleForm.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class simpleForm extends testBase {
+
+    @Test
+    void onlyUserName() {
+        open("/text-box");
+        $("[id=userName]").setValue("Довольно");
+        $("[id=submit]").click();
+
+        $(".mb-1").shouldHave(text("Довольно"));
+    }
+
+    @Test
+    void incorrectEmail() {
+        open("/text-box");
+        $("[id=userEmail]").setValue("123@qwe/ru");
+        $("[id=submit]").click();
+
+        $("[id=userEmail]").shouldHave(cssClass("field-error" ));
+    }
+}
